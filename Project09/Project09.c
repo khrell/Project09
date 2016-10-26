586b77b162a633cea3857df727c390f6822fccd7
riptorn01@gmail.com <Trinity1>
2016-10-26 13:53:57 -0500

    Rev 1 Basic Layout

---
+++ Project09/Project09.c
@@ -0,0 +1,39 @@
+/*
+
+Name: Program09
+
+Programmer: Shawn San Miguel
+
+Class: COSC 1336.001
+
+Date: 10-26-16
+
+*/
+
+#include <stdio.h>
+
+
+
+
+int main()
+{
+
+
+	return 0;
+}
+
+
+int strToFloat()
+{
+
+
+
+
+}
+
+int findstring()
+{
+
+
+
+}
